764641d0a7a0752c95e4691bbddf0c121d7997ca
Daniela Guillen <daniela@redpixel.io>
2022-11-02 18:10:44 -0400

    Add services section

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,7 +21,12 @@ module.exports = {
 				"solarBlue": "#0a3e76",
 				"solarBlack": "#111b27",
 				"solarGray": "#ebebeb",
-			}
+			},
+			boxShadow: {
+				DEFAULT: '0px 0px 15px 0px rgba(0, 0, 0, 0.07)',
+				wide: '-9.192px -9.192px 57px 0px rgba(0, 0, 0, 0.3)',
+				small: '-4px -4px 57px 0px rgba(0, 0, 0, 0.3)',
+			},
 		}
   },
   plugins: [
